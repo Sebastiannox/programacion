19bb7332646c91af91d7df9164471a8e69006232
Sebastiannox <sebastian.suazo01@gmail.com>
2022-05-03 13:49:56 -0400

    cambios en los archivos index

--- myfirstapp/src/index2.js
+++ myfirstapp/src/index2.js
@@ -1,79 +1,198 @@
-import React from 'react'
-import ReactDOM, { render } from 'react-dom'
-const container = document.getElementById('root');
-const numbers = [1,2,3,4,5];
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+const container = document.getElementById("root");
+const numbers = [1, 2, 3, 4, 5];
 const doubled = numbers.map((numbers) => numbers * 2);
 console.log(doubled);
-class LoginControl extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleLoginClick = this.handleLoginClick.bind(this);
-      this.handleLogoutClick = this.handleLogoutClick.bind(this);
-      this.state = {isLoggedIn: false};
-    }
-  
-    handleLoginClick() {
-      this.setState({isLoggedIn: true});
-    }
-  
-    handleLogoutClick() {
-      this.setState({isLoggedIn: false});
-    }
-  
-    render() {
-      const isLoggedIn = this.state.isLoggedIn;
-      let button;
-  
-      if (isLoggedIn) {
-        button = <LogoutButton onClick={this.handleLogoutClick} />;
-      } else {
-        button = <LoginButton onClick={this.handleLoginClick} />;
-      }
-  
-      return (
-        <div>
-          <Greeting isLoggedIn={isLoggedIn} />
-          {button}
-        </div>
-      );
-    }
-}  
-function UserGreeting(props) {
-    return <h1>Welcome back!</h1>;
-}
-  
-function GuestGreeting(props) {
-    return <h1>Please sign up.</h1>;
-}
-  
-function Greeting(props) {
-    const isLoggedIn = props.isLoggedIn;
-    if (isLoggedIn) {
-      return <UserGreeting />;
-    }
-    return <GuestGreeting />;
-}
-  
-function LoginButton(props) {
-    return (
-      <button onClick={props.onClick}>
-        Login
-      </button>
-    );
-}
-  
-function LogoutButton(props) {
-    return (
-      <button onClick={props.onClick}>
-        Logout
-      </button>
-    );
-}
-  
-ReactDOM.render(
-    <LoginControl />,
-    document.getElementById('root')
-);
-  
+const paddockManagers = [
+  { id: 1, taxNumber: "132254524", name: "JUAN TAPIA BURGOS" },
+  { id: 2, taxNumber: "143618668", name: "EFRAIN SOTO VERA" },
+  { id: 3, taxNumber: "78903228", name: "CARLOS PEREZ GONZALEZ" },
+  { id: 4, taxNumber: "176812737", name: "ANDRES VIÑALES CIENFUEGOS" },
+  { id: 5, taxNumber: "216352696", name: "OSCAR PEREZ ZUÑIGA" },
+  { id: 6, taxNumber: "78684747", name: "JOAQUIN ANDRADE SANDOVAL" },
+];
+const paddockType = [
+  { id: 1, name: "PALTOS" },
+  { id: 2, name: "AVELLANOS" },
+  { id: 3, name: "CEREZAS" },
+  { id: 4, name: "NOGALES" },
+];
 
-
+const farms = [
+  { id: 1, name: "AGRICOLA SANTA ANA" },
+  { id: 2, name: "VINA SANTA PAULA" },
+  { id: 3, name: "FORESTAL Y AGRICOLA LO ENCINA" },
+];
+const paddocks = [
+  {
+    paddockManagerId: 6,
+    farmId: 1,
+    paddockTypeId: 1,
+    harvestYear: 2019,
+    area: 1200,
+  },
+  {
+    paddockManagerId: 1,
+    farmId: 3,
+    paddockTypeId: 4,
+    harvestYear: 2019,
+    area: 500,
+  },
+  {
+    paddockManagerId: 5,
+    farmId: 3,
+    paddockTypeId: 2,
+    harvestYear: 2020,
+    area: 20000,
+  },
+  {
+    paddockManagerId: 2,
+    farmId: 2,
+    paddockTypeId: 3,
+    harvestYear: 2021,
+    area: 8401,
+  },
+  {
+    paddockManagerId: 3,
+    farmId: 1,
+    paddockTypeId: 1,
+    harvestYear: 2020,
+    area: 2877,
+  },
+  {
+    paddockManagerId: 5,
+    farmId: 2,
+    paddockTypeId: 2,
+    harvestYear: 2017,
+    area: 15902,
+  },
+  {
+    paddockManagerId: 3,
+    farmId: 3,
+    paddockTypeId: 2,
+    harvestYear: 2018,
+    area: 1736,
+  },
+  {
+    paddockManagerId: 2,
+    farmId: 3,
+    paddockTypeId: 3,
+    harvestYear: 2020,
+    area: 2965,
+  },
+  {
+    paddockManagerId: 4,
+    farmId: 3,
+    paddockTypeId: 4,
+    harvestYear: 2018,
+    area: 1651,
+  },
+  {
+    paddockManagerId: 5,
+    farmId: 1,
+    paddockTypeId: 1,
+    harvestYear: 2018,
+    area: 700,
+  },
+  {
+    paddockManagerId: 1,
+    farmId: 2,
+    paddockTypeId: 1,
+    harvestYear: 2019,
+    area: 7956,
+  },
+  {
+    paddockManagerId: 5,
+    farmId: 3,
+    paddockTypeId: 2,
+    harvestYear: 2020,
+    area: 3745,
+  },
+  {
+    paddockManagerId: 6,
+    farmId: 1,
+    paddockTypeId: 3,
+    harvestYear: 2021,
+    area: 11362,
+  },
+  {
+    paddockManagerId: 2,
+    farmId: 3,
+    paddockTypeId: 3,
+    harvestYear: 2021,
+    area: 300,
+  },
+  {
+    paddockManagerId: 3,
+    farmId: 2,
+    paddockTypeId: 2,
+    harvestYear: 2020,
+    area: 19188,
+  },
+  {
+    paddockManagerId: 3,
+    farmId: 1,
+    paddockTypeId: 1,
+    harvestYear: 2019,
+    area: 17137,
+  },
+  {
+    paddockManagerId: 4,
+    farmId: 3,
+    paddockTypeId: 2,
+    harvestYear: 2020,
+    area: 100,
+  },
+  {
+    paddockManagerId: 2,
+    farmId: 1,
+    paddockTypeId: 3,
+    harvestYear: 2019,
+    area: 11845,
+  },
+  {
+    paddockManagerId: 5,
+    farmId: 2,
+    paddockTypeId: 1,
+    harvestYear: 2018,
+    area: 15969,
+  },
+  {
+    paddockManagerId: 1,
+    farmId: 3,
+    paddockTypeId: 1,
+    harvestYear: 2029,
+    area: 10420,
+  },
+  {
+    paddockManagerId: 5,
+    farmId: 2,
+    paddockTypeId: 3,
+    harvestYear: 2010,
+    area: 3200,
+  },
+  {
+    paddockManagerId: 6,
+    farmId: 1,
+    paddockTypeId: 2,
+    harvestYear: 2012,
+    area: 10587,
+  },
+  {
+    paddockManagerId: 2,
+    farmId: 2,
+    paddockTypeId: 2,
+    harvestYear: 2018,
+    area: 16750,
+  },
+];
+export function ListPaddockManagerIds() {
+  let arrayIdPaddockManager = paddockManagers.map((paddockManager) => paddockManager.id);
+  console
+  return arrayIdPaddockManager;
+}
+export function ListPaddockManagersByName() {
+  return paddockManagers.map((paddockManager) =>paddockManager.taxNumber);
+  
+}
